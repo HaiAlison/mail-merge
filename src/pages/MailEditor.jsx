bdda2dcdd373932e91b6b035a6662a2e41cfd4bd
haialison <hai.mai@unibiz.io>
2024-11-20 00:21:24 +0700

    feature/send-mail

--- src/pages/MailEditor.jsx
+++ src/pages/MailEditor.jsx
@@ -1,80 +1,47 @@
-import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import SunEditor from "suneditor-react"; // Or use fetch API if you prefer
+import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
+import { notification } from "antd";
 
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic"; // Or use fetch API if you prefer
-
-const MailEditor = () => {
-  const handleEditorChange = (event, editor) => {
-    const data = editor.getData();
-    console.log('Content:', data);
-
-    // Send the data to your backend
-    //   axios.post('/api/save-email-content', { content: data })
-    //     .then(response => {
-    //       console.log('Content saved successfully:', response.data);
-    //     })
-    //     .catch(error => {
-    //       console.error('Error saving content:', error);
-    //     });
-  };
-  ClassicEditor
-    .create(document.querySelector('#editor'), {
-      fontFamily: {
-        options:[],
-        supportAllValues: true
+const MailEditor = forwardRef((props, ref) => {
+    const editorRef = useRef(null);
+    useImperativeHandle(ref, () => ({
+      getContent: () => {
+        return editorRef.current.editor.getContents();
+      }
+    }));
+    const options = {
+      buttonList: [
+        ['undo', 'redo'],
+        ['font', 'fontSize', 'formatBlock'],
+        ['bold', 'italic', 'underline'],
+        ['fontColor', 'hiliteColor'],
+        ['removeFormat'],
+        ['outdent', 'indent'],
+        ['align', 'list', 'lineHeight'],
+        ['link', 'image'],
+        ['fullScreen', 'codeView'],
+        ['preview', 'print'],
+        ['save']
+      ],
+      callBackSave: (contents) => {
+        console.log('Save:', contents);
+        notification.success({
+          message: 'Nội dung đã được lưu',
+          description: 'Nội dung email đã được lưu thành công.',
+        });
       },
-      toolbar: [
-        'heading', 'bulletedList', 'numberedList', 'fontFamily', 'undo', 'redo'
-      ]
-    })
-    .then(editor => {
-      // You can access the editor instance here
-      editor.setData('Hello, world!');
-      console.log(editor);
-    })
-    .catch(error => {
-      console.error('There was a problem initializing the editor:', error);
-    });
-  return (
-    <div>
-      <CKEditor
-        id="editor"
-        editor={ClassicEditor}
-        data=""
-        onChange={handleEditorChange}
-        config={{
-          toolbar: [
-            'heading', '|',
-            'bold', 'italic', 'underline', 'strikethrough', '|',
-            'fontSize', 'fontFamily', '|',
-            'bulletedList', 'numberedList', '|',
-            'link', 'blockQuote', 'undo', 'redo', '|',
-            'alignment', 'indent', 'outdent', '|',
-            'imageUpload', 'mediaEmbed', 'insertTable', '|',
-            'code', 'codeBlock', 'removeFormat'
-          ],
-          fontSize: {
-            options: ['tiny', 'small', 'default', 'big', 'huge']
-          },
-          fontFamily: {
-            options: [
-              'default',
-              'Arial, Helvetica, sans-serif',
-              'Courier New, Courier, monospace',
-              'Georgia, serif',
-              'Lucida Sans Unicode, Lucida Grande, sans-serif',
-              'Tahoma, Geneva, sans-serif',
-              'Times New Roman, Times, serif',
-              'Trebuchet MS, Helvetica, sans-serif',
-              'Verdana, Geneva, sans-serif'
-            ]
-          },
-          alignment: {
-            options: ['left', 'center', 'right', 'justify']
-          }
-        }}/>
-    </div>
-  );
-};
+      width: '100%',
+      height: '200px',
+      stickyToolbar: '0',
+      maxHeight: '250px',
+    }
+    return (
+      <div>
+        <SunEditor ref={editorRef} onChange={props.onChange} setOptions={options}/>
+      </div>
+    );
+  })
+;
 
 export default MailEditor;
--- src/pages/MailForm.jsx
+++ src/pages/MailForm.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useRef, useState } from 'react';
+import { Form, Input, Button, notification } from 'antd';
 import Home from "./Home";
 import MailEditor from "./MailEditor";
+import { sendMail } from "../api/mailApi";
 
 const MailForm = () => {
   const [form] = Form.useForm();
+  const editorRef = useRef(null);
 
   const onFinish = (values) => {
-    console.log('Gửi dữ liệu:', values);
-    // Gửi yêu cầu email của bạn tại đây, ví dụ với axios hoặc fetch
+    try {
+      const payload = {
+        user_id: '7c29d409-c134-44fb-b171-4d942006979a',
+        to: values.to,
+        subject: values.subject,
+        text: values.body,
+      };
+      console.log(payload)
+      sendMail(payload).then(() => {
+        notification.success({
+          message: 'Thành công',
+          description: 'Email đã được gửi thành công.',
+        });
+        form.resetFields();
+        editorRef?.current?.editor?.setContents('');
+      });
+    } catch (e) {
+      console.error(e);
+      notification.error({
+        message: 'Lỗi',
+        description: 'Có lỗi xảy ra khi gửi email.',
+      })
+    }
   };
+  const [loadings, setLoadings] = useState([]);
+  const enterLoading = (index) => {
+    setLoadings((prevLoadings) => {
+      const newLoadings = [...prevLoadings];
+      newLoadings[index] = true;
+      return newLoadings;
+    });
+
+    setTimeout(() => {
+      setLoadings((prevLoadings) => {
+        const newLoadings = [...prevLoadings];
+        newLoadings[index] = false;
+        return newLoadings;
+      });
+    }, 1000);
+  };
+
 
   return (
     <Home>
@@ -39,12 +79,10 @@ const MailForm = () => {
           label="Nội dung"
           rules={[{required: true, message: 'Vui lòng nhập nội dung!'}]}
         >
-
-          <MailEditor/>
+          <MailEditor ref={editorRef}/>
         </Form.Item>
-
         <Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" loading={loadings[1]} onClick={() => enterLoading(1)} htmlType="submit">
             Gửi
           </Button>
         </Form.Item>
